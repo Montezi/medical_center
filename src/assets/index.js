223084161f8ee5d5a39294f27b55291e1f81d7a1
Gaby <lubaaenzin@gmail.com>
2020-09-04 01:46:20 -0300

    login screen --WIP

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import LogoHorizontal from './logoHorizontal.png';
+import LogoVertical from './logoVertical.png';
+import BackgroundLogin from './backgroundLogin.png';
+import agenda from './agenda.png';
+import estetoscopio from './estetoscopio.png';
+import logout from './logout.png';
+import maleta from './maleta.png';
+import pasta from './pasta.png';
+
+export {
+  LogoHorizontal,
+  LogoVertical,
+  BackgroundLogin,
+  agenda,
+  estetoscopio,
+  logout,
+  pasta,
+  maleta,
+};
