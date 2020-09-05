9c3b8c6bec0c50e0303a9589aace550da13b7618
mirian_lima <mirian.lima@stormsec.com.br>
2020-09-04 22:17:27 -0300

    fix padding top ios

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
-import { default as theme } from './custom-theme.json';
+import * as theme from './custom-theme.json';
 import Routes from './src/pages/routes';
 
 const App = () => {
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { StyleSheet, ImageBackground, Image } from 'react-native';
+import {
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Platform,
+  NativeModules,
+} from 'react-native';
+
 import { Layout, Text } from '@ui-kitten/components';
 import { BackgroundLogin, LogoVertical } from '../../assets';
+
+const { StatusBarManager } = NativeModules;
+
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 40 : StatusBarManager.HEIGHT;
 
 const Login = () => {
   return (
     <Layout style={styles.container} level="1">
       <ImageBackground source={BackgroundLogin} style={styles.image}>
         <Image source={LogoVertical} style={{ width: 205, height: 180 }} />
-        <Text style={styles.title} status="primary">cadastrar</Text>
+        <Text style={styles.title} status="primary">
+          cadastrar
+        </Text>
       </ImageBackground>
     </Layout>
   );
@@ -20,6 +33,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: STATUSBAR_HEIGHT,
   },
   image: {
     flex: 1,
@@ -35,8 +49,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     marginTop: 20,
-    textTransform: "uppercase",
-    fontWeight: "700",
+    textTransform: 'uppercase',
+    fontWeight: '700',
   },
 });
 
