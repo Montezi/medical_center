17dad818cba213eb0fbd99725b5948ebc9ea7c30
mirian_lima <mirian.lima@stormsec.com.br>
2020-09-07 22:08:21 -0300

    merge && splash screen

--- App.js
+++ App.js
@@ -6,12 +6,16 @@ import { Provider } from 'react-redux';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import SplashScreen from 'react-native-splash-screen';
 import { store, persistor } from './src/store';
 import * as theme from './custom-theme.json';
 import * as mapping from './mapping.json';
 import Routes from './src/pages/routes';
 
 const App = () => {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <>
       <Provider store={store}>
